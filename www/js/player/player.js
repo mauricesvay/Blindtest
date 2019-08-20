82d6e8e1ccfd90d3120782419fbfce9029bca315
Maurice Svay <maurice@sqreen.io>
2019-08-20 19:42:46 +0200

    skip when all players give wrong answers

--- www/js/player/player.js
+++ www/js/player/player.js
@@ -1,85 +1,87 @@
 var socket;
 var App = {
-    currentSong: null,
-    username: null,
+  currentSong: null,
+  username: null,
 
-    init: function() {
-        socket = io.connect("http://" + location.hostname + ":" + location.port);
+  init: function() {
+    socket = io.connect("http://" + location.hostname + ":" + location.port);
 
-        //Login
-        $("form").on("submit", function(e) {
-            e.preventDefault();
-            var username = $("#nickname").val();
-            App.username = username;
-            $("#username").text(username);
-            socket.emit("login", {
-                username: username
-            });
+    //Login
+    $("form").on("submit", function(e) {
+      e.preventDefault();
+      var username = $("#nickname").val();
+      App.username = username;
+      $("#username").text(username);
+      socket.emit("login", {
+        username: username
+      });
+    });
+
+    socket.on("login", function() {
+      $("form").hide();
+      $("#suggestions").html('<span class="wait">Ready for next round</span>');
+    });
+
+    socket.on("refused", function() {
+      alert("Server is full");
+      socket.disconnect();
+    });
+
+    socket.on("song", function(song) {
+      // if (App.userName) {
+      App.onSong(song);
+      // }
+    });
+
+    socket.on("answer", function(data) {
+      if (data && data.username === App.username) {
+        App.onRight();
+      } else {
+        App.onWrong();
+      }
+    });
+
+    //Suggestions
+    var eventType =
+      typeof window.ontouchstart === "undefined" ? "click" : "tap";
+    $("#suggestions").on(eventType, function(e) {
+      if ($(e.target).attr("data-value")) {
+        var value = $(e.target).attr("data-value");
+        socket.emit("button", {
+          button: value
         });
+      }
+    });
+  },
 
-        socket.on("login", function() {
-            $("form").hide();
-            $("#suggestions").html('<span class="wait">Ready for next round</span>');
-        });
+  onSong: function(song) {
+    App.currentSong = song.song;
+    App.showSuggestions();
+  },
 
-        socket.on("refused", function() {
-            alert("Server is full");
-            socket.disconnect();
-        });
+  onRight: function() {
+    $("#suggestions").html("<span class='right'>✅</span>");
+  },
 
-        socket.on("song", function(song) {
-            // if (App.userName) {
-            App.onSong(song);
-            // }
-        });
+  //@TODO : add case when other user is right onOtherRight
 
-        socket.on("right", function(data) {
-            if (data) {
-                App.onWrong();
-            } else {
-                App.onRight();
-            }
-        });
+  onWrong: function() {
+    $("#suggestions").html("<span class='wrong'>❌</span>");
+  },
 
-        socket.on("wrong", function() {
-            App.onWrong();
-        });
-
-        //Suggestions
-        var eventType = typeof window.ontouchstart === "undefined" ? "click" : "tap";
-        $("#suggestions").on(eventType, function(e) {
-            if ($(e.target).attr("data-value")) {
-                var value = $(e.target).attr("data-value");
-                socket.emit("button", {
-                    button: value
-                });
-            }
-        });
-    },
-
-    onSong: function(song) {
-        App.currentSong = song.song;
-        App.showSuggestions();
-    },
-
-    onRight: function() {
-        $("#suggestions").html("<span class='right'>✔</span>");
-    },
-
-    //@TODO : add case when other user is right onOtherRight
-
-    onWrong: function() {
-        $("#suggestions").html("<span class='wrong'>✖</span>");
-    },
-
-    showSuggestions: function() {
-        var out = "";
-        for (var i = 0, l = App.currentSong.suggestions.length; i < l; i++) {
-            out += '<li data-value="' + i + '">' + App.currentSong.suggestions[i].name + "</li>";
-        }
-        $("#suggestions").html("<ul>" + out + "</ul>");
+  showSuggestions: function() {
+    var out = "";
+    for (var i = 0, l = App.currentSong.suggestions.length; i < l; i++) {
+      out +=
+        '<li data-value="' +
+        i +
+        '">' +
+        App.currentSong.suggestions[i].name +
+        "</li>";
     }
+    $("#suggestions").html("<ul>" + out + "</ul>");
+  }
 };
 $(function() {
-    App.init();
+  App.init();
 });
